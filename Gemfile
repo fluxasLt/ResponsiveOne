aec801f669ab7cd635a4a99feffba7400e7f3bb0
Vygantas Lungis <VygantasLungis@gmail.com>
2015-09-22 21:53:01 +0300

    Breakpoint and Bundler install, config.rb creation

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+gem 'sass', '~>3.4.18'
+gem 'compass', '~>1.1.0.alpha.3'
+gem 'breakpoint', '~>2.5.0'
---
+++ config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'files/css'
+sass_dir = 'files/sass'
+images_dir = 'images'
+javascripts_dir = 'files/javascript'
+relative_assets = true
+line_comments = true
+output_style = :compressed
+require 'breakpoint'
