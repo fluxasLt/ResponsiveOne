0c2458b95178fb02d6a662eb4edea157f57267b3
Vygantas Lungis <VygantasLungis@gmail.com>
2015-09-27 00:27:27 +0300

    HTML markup, color and font variables

--- config.rb
+++ config.rb
@@ -4,6 +4,7 @@ css_dir = 'files/css'
 sass_dir = 'files/sass'
 images_dir = 'images'
 javascripts_dir = 'files/javascript'
+fonts_dir = 'files/fonts'
 relative_assets = true
 line_comments = true
 output_style = :compressed
