af7fa7bc7361c904ba2959ec9dfe1118a409bac9
Vygantas Lungis <VygantasLungis@gmail.com>
2015-10-21 01:01:09 +0300

    SEO add

--- files/javascript/script.js
+++ files/javascript/script.js
@@ -4,11 +4,9 @@ $(document).ready(function() {
 	$(window).resize(function() {
 		burgerMenuDisplayChange(); // Burger menu display changes.
 		centerImage();            // Center image.
-		
 	});
     burgerMenuClick();   // Burger menu clicking.
     previewGallery();   // Preview image.
     scrollToSection(); // Smooth scroll then clicked on nav link.
     makeSkillsBars(); // Makes skills bars.
-    
 });
